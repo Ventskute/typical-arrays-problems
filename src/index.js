022c7a0669b4a05f48289e1fd62fd2ff4d4845b0
Viktoryia Ventskute <ventskute.vika@gmail.com>
2020-10-03 14:52:41 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  let minValue = array[0];
+  let currentValue = 0;
+  for (let i = 0; i < array.length; i++) {
+    currentValue = array[i];
+    if (currentValue < minValue) {
+      minValue = currentValue;
+    }
+  }
+  return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  let maxValue = array[0];
+  let currentValue = 0;
+  for (let i = 0; i < array.length; i++) {
+    currentValue = array[i];
+    if (currentValue > maxValue) {
+      maxValue = currentValue;
+    }
+  }
+  return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum/array.length;
 }
